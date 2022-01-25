804495d33437b7067a1a4059b871b788181b08b1
Gunaseelan Rajamanickam (MS/ESV31-CC) <ugn1cob@bosch.com>
2022-01-25 11:26:05 +0530

    assertion included in the production code

--- misaligned.py
+++ misaligned.py
@@ -1,3 +1,4 @@
+printed_list = []
 
 def print_color_map():
     major_colors = ["White", "Red", "Black", "Yellow", "Violet"]
@@ -5,9 +6,14 @@ def print_color_map():
     for i, major in enumerate(major_colors):
         for j, minor in enumerate(minor_colors):
             print(f'{i * 5 + j} | {major} | {minor}')
-    return len(major_colors) * len(minor_colors)
+            printed_list.append(f'{i * 5 + j} | {major} | {minor}')
+    return len(major_colors) * len(minor_colors), printed_list
 
 
-result = print_color_map()
+result, printed_list = print_color_map()
 assert(result == 25)
+test_string_first = printed_list[0]
+test_string_last = printed_list[-1]
+assert(test_string_first.find('|') == test_string_last.find('|'))
+assert(len(test_string_first.split(' | ')[0]) == len(test_string_last.split(' | ')[0]))
 print("All is well (maybe!)\n")
